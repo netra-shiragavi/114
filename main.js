258ca19a257f79c43fb3bcaabcd66d395409dd9a
netra-shiragavi <82329541+netra-shiragavi@users.noreply.github.com>
2023-06-19 19:37:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+  
+  
+  }
+  
+  function preload(){
+  
+  }
+  function draw(){
+   image(video, 0,0,300,300);
+  }
+  
+  function Snapshot(){
+      save("My_pic.png");
+  }
